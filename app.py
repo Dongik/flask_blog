91a692401584e70493451587abc3d306e29ff4c8
Dongik <dongik3475@gmail.com>
2017-10-03 00:25:15 +0900

    completement without db and Korean

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+
+db = SQLAlchemy(app)
+
+class Blogpost(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(50))
+	subtitle = db.Column(db.String(50))
+	author = db.Column(db.String(20))
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 @app.route('/')
 def index():
@@ -17,6 +37,10 @@ def post():
 @app.route('/contact')
 def contact():
 	return render_template('contact.html')
+
+@app.route('/add')
+def add():
+	return render_template('add.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
