d6c02de3420fd9d3054ff307c617ee6844cafd9a
Dongik <dongik3475@gmail.com>
2017-10-03 21:42:37 +0900

    fix db problem

--- app.py
+++ app.py
@@ -2,24 +2,24 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-
-db = SQLAlchemy(app)
-
-class Blogpost(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	title = db.Column(db.String(50))
-	subtitle = db.Column(db.String(50))
-	author = db.Column(db.String(20))
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
+#
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+#
+# db = SQLAlchemy(app)
+#
+# class Blogpost(db.Model):
+# 	id = db.Column(db.Integer, primary_key=True)
+# 	title = db.Column(db.String(50))
+# 	subtitle = db.Column(db.String(50))
+# 	author = db.Column(db.String(20))
+#
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(80), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#
+#     def __repr__(self):
+#         return '<User %r>' % self.username
 
 @app.route('/')
 def index():
