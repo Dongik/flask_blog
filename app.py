fbfabd67532f4c3a4213c28c45f28ce8f7c86680
Dongik <dongik3475@gmail.com>
2017-10-08 13:24:33 +0900

    change index and added posting

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from pymongo import MongoClient # Database Connector
 from bson.objectid import ObjectId
+from datetime import datetime
 
 client = MongoClient('39.162.99.200', 27017)
 
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////mnt/c/User/mySpace/flask_blog/blog.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////mnt/c/home/dongik/mySpace/flask_blog/blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/dongik/mySpace/flask_blog/blog.db'
 
 db = SQLAlchemy(app)
 
@@ -20,27 +22,23 @@ class Blogpost(db.Model):
 	date_posted = db.Column(db.DateTime)
 	content = db.Column(db.Text)
 
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-#
-#     def __repr__(self):
-#         return '<User %r>' % self.username
-
-
 
 @app.route('/')
 def index():
-	return render_template('index_webgl.html')
+	posts = Blogpost.query.all()
+
+	return render_template('index_old.html', posts=posts)
 
 @app.route('/about')
 def about():
 	return render_template('about.html')
 
-@app.route('/post')
-def post():
-	return render_template('post.html')
+@app.route('/post/<int:post_id>')
+def post(post_id):
+	post = Blogpost.query.filter_by(id=post_id).one()
+	date_posted = post.date_posted.strftime('%B %d, %Y')
+
+	return render_template('post.html',post=post, date_posted=date_posted)
 
 @app.route('/contact')
 def contact():
@@ -57,8 +55,12 @@ def addpost():
 	author = request.form['author']
 	content = request.form['content']
 
-	return '<h1>Title: {} Subtitle: {} Author: {} Content: {}</h1>'.format(title, subtitle, author, content)
+	post = Blogpost(title=title, subtitle=subtitle, author=author, content=content, date_posted=datetime.now())
 
+	db.session.add(post)
+	db.session.commit()
+
+	return redirect(url_for('index'))
 
 if __name__ == '__main__':
 	app.run(debug=True)
